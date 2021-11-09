07181760ac128abc423ac6bed0e3d444567cf4b6
ed-vieira <edfelip@gmail.com>
2021-11-09 13:32:31 -0300

    containers

---
+++ src/containers/DefaultAside.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-tabs>
+    <b-tab>
+      <template slot="title">
+        <i class='icon-list'></i>
+      </template>
+      <b-list-group class="list-group-accent">
+      </b-list-group>
+    </b-tab>
+    <b-tab>
+      <template slot="title">
+        <i class='icon-speech'></i>
+      </template>
+      <div class="p-3">
+      </div>
+    </b-tab>
+    <b-tab>
+      <template slot="title">
+        <i class='icon-settings'></i>
+      </template>
+      <div class="p-3">
+
+      </div>
+    </b-tab>
+  </b-tabs>
+</template>
+
+<script>
+import { Switch as cSwitch } from '@coreui/vue'
+export default {
+  name: 'DefaultAside',
+  components: {
+    cSwitch
+  }
+}
+</script>
---
+++ src/containers/DefaultContainer.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="app">
+    <AppHeader fixed>
+      <SidebarToggler class="d-lg-none" display="md" mobile />
+      <b-link class="navbar-brand" to="#">
+        <div class="navbar-brand-full">Accordous</div>
+        <div class="navbar-brand-minimized">AC</div>
+      </b-link>
+      <SidebarToggler class="d-md-down-none" display="lg" />
+      <b-navbar-nav class="d-md-down-none">
+        <b-nav-item class="px-3" to="/dashboard" exact>Home</b-nav-item>
+        <b-nav-item class="px-3" to="/dashboard/properties" exact>Imóveis</b-nav-item>
+        <b-nav-item class="px-3" to="/dashboard/contracts" exact>Contratos</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-bell"></i>
+          <b-badge pill variant="danger"></b-badge>
+        </b-nav-item>
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-list"></i>
+        </b-nav-item>
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-location-pin"></i>
+        </b-nav-item>
+        <DefaultHeaderDropdownAccnt/>
+      </b-navbar-nav>
+      <AsideToggler class="d-none d-lg-block" />
+      <!--<AsideToggler class="d-lg-none" mobile />-->
+    </AppHeader>
+    <div class="app-body">
+      <AppSidebar fixed>
+        <SidebarHeader/>
+        <SidebarForm/>
+        <SidebarNav :navItems="nav"></SidebarNav>
+        <SidebarFooter/>
+        <SidebarMinimizer/>
+      </AppSidebar>
+      <main class="main">
+        <Breadcrumb :list="list"/>
+        <div class="container-fluid">
+          <router-view></router-view>
+        </div>
+      </main>
+      <AppAside fixed>
+        <!--aside-->
+        <DefaultAside/>
+      </AppAside>
+    </div>
+    <TheFooter>
+      <!--footer-->
+      <div class="float-right">
+      </div>
+      <div class="ml-auto">
+      </div>
+    </TheFooter>
+  </div>
+</template>
+
+<script>
+import nav from '@/_nav'
+import { Header as AppHeader, SidebarToggler, Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav, Aside as AppAside, AsideToggler, Footer as TheFooter, Breadcrumb } from '@coreui/vue'
+import DefaultAside from './DefaultAside'
+import DefaultHeaderDropdownAccnt from './DefaultHeaderDropdownAccnt'
+
+export default {
+  name: 'full',
+  components: {
+    AsideToggler,
+    AppHeader,
+    AppSidebar,
+    AppAside,
+    TheFooter,
+    Breadcrumb,
+    DefaultAside,
+    DefaultHeaderDropdownAccnt,
+    SidebarForm,
+    SidebarFooter,
+    SidebarToggler,
+    SidebarHeader,
+    SidebarNav,
+    SidebarMinimizer
+  },
+  data () {
+    return {
+      nav: nav.items
+    }
+  },
+  computed: {
+    name () {
+      return this.$route.name
+    },
+    list () {
+      return this.$route.matched.filter((route) => route.name || route.meta.label )
+    }
+  }
+}
+</script>
